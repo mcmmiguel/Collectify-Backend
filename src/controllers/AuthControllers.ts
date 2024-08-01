dcffd39540a892e2d9ac0d56ecf751c78650867c
Miguel Cobian <miguelcobian99@gmail.com>
2024-08-01 13:09:48 -0600

    Feat. Hash password when register user

--- src/controllers/AuthControllers.ts
+++ src/controllers/AuthControllers.ts
@@ -1,13 +1,20 @@
 import { Request, Response } from 'express'
 import User from '../model/User';
-
-
+import { hashPassword } from '../utils/auth';
 class AuthController {
     static registerUser = async (req: Request, res: Response) => {
         try {
             const { email, password } = req.body;
 
+            const userExists = await User.findOne({ email });
+            if (userExists) {
+                const error = new Error('This email is already registered');
+                return res.status(409).json({ error: error.message });
+            }
+
             const user = new User(req.body);
+
+            user.password = await hashPassword(password);
 
             await user.save();
 
