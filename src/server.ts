fffd997b1dc7ba2ef3050fc5599c810d61ffc3c2
Miguel Cobian <miguelcobian99@gmail.com>
2024-08-06 21:13:30 -0600

    Feat. cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 import { connectDB } from './config/db';
@@ -8,6 +9,7 @@ import adminRouter from './routes/adminRoutes';
 import authRouter from './routes/authRoutes';
 import collectionRouter from './routes/collectionRoutes';
 import publicRouter from './routes/publicRoutes';
+import { corsConfig } from './config/cors';
 
 const server = express();
 
@@ -20,6 +22,8 @@ connectDB();
 
 // Read json from form
 server.use(express.json());
+
+server.use(cors(corsConfig));
 
 server.use('/api/auth', authRouter);
 server.use('/api/collections', collectionRouter);
